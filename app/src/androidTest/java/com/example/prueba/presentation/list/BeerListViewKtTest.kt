04742ed927876fd49e8cf527266b3a7d1908458f
n158384 <fran.munoz-odsbank@gmail.com>
2023-08-10 00:56:44 +0200

    pendientes: fixear tests y avs. Y listo

--- app/src/androidTest/java/com/example/prueba/presentation/list/BeerListViewKtTest.kt
+++ app/src/androidTest/java/com/example/prueba/presentation/list/BeerListViewKtTest.kt
@@ -1,4 +1,87 @@
 package com.example.prueba.presentation.list
 
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import org.junit.Rule
+import org.junit.Test
+import java.io.FileDescriptor
 
-class BeerListViewKtTest{}
+
+class BeerListViewKtTest{
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    fun generateFakeBeerList(): List<Beer>{
+        return listOf(
+            Beer("1", "Beer Name 1", "Description 1"),
+            Beer("2", "Beer Name 2", "Description 2")
+            )
+    }
+
+    @Test
+    fun `WHEN view its crated it has its UI elements wo ITEM`(){
+        composeTestRule.setContent {
+            BeerListView(
+                onDetailClick ={},
+                beerListViewModel = FakeBeerListViewModel())
+        }
+
+        composeTestRule.onNodeWithText("Beer Application").assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription("informacion").assertIsDisplayed()
+    }
+
+    @Test
+    fun `WHEN view its created it has its UI elements and ITEM`(){
+        val fakeViewModel = FakeBeerListViewModel()
+        fakeViewModel.beerList.value = generateFakeBeerList()
+
+        composeTestRule.setContent {
+            BeerListView(
+                onDetailClick = {},
+                beerListViewModel = fakeViewModel
+            )
+        }
+
+        composeTestRule.onAllNodesWithTag("BeerItem").assertCountEquals(2)
+    }
+
+    // we using a object with id of one
+    // we may do more checks if need
+    @Test
+    fun `WHEN its clicked it goes there`(){
+        var clickedItemId: String? = null
+        val fakeViewModel = FakeBeerListViewModel()
+        fakeViewModel.beerList.value = generateFakeBeerList()
+
+        composeTestRule.setContent {
+            BeerListView(
+                onDetailClick = {itemId -> clickedItemId = itemId},
+                beerListViewModel = fakeViewModel
+            )
+        }
+
+        composeTestRule.onAllNodesWithTag("BeerItem")[0].performClick()
+
+        assert( clickedItemId== "1")
+    }
+
+
+}
+
+// auxiliar
+
+data class Beer(
+    val id: String,
+    val name: String,
+    val description: String
+)
+class FakeBeerListViewModel : BeerListViewModel(){
+    val beerList = mutableStateOf<List<Beer?>(null)
+}
+
--- app/src/test/java/com/example/prueba/domain/usecases/GetBeerUsecaseTest.kt
+++ app/src/test/java/com/example/prueba/domain/usecases/GetBeerUsecaseTest.kt
@@ -1,4 +1,40 @@
 package com.example.prueba.domain.usecases
 
+import com.example.prueba.data.repository.BeerRepositoryInterface
+import com.example.prueba.domain.model.BeerDetailModel
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+//then return?
+// what dependency me falta?
+
 class GetBeerUsecaseTest {
+
+    @Mock
+    private lateinit var beerRepositoryInterface: BeerRepositoryInterface
+
+    private lateinit var getBeerUsecase: GetBeerUsecase
+
+    @Before
+    fun settingUp(){
+        MockitoAnnotations.initMocks(this)
+        getBeerUsecase = GetBeerUsecase(beerRepositoryInterface)
+    }
+
+    @Test
+    fun `WHEN use invoke should return BeerDetailModel`() = runBlocking {
+        val beerId = "fake_id"
+        val expectedBeerDetail = BeerDetailModel(
+            "1", "name", "tagline", "firstBrewed", "description", "imageUrl")
+
+        `when(beerRepositoryInterface.getBeerById(beerId).thenReturn(expectedBeerDetail))
+
+        val result = getBeerUsecase.invoke(beerId)
+
+        assert(result== expectedBeerDetail)
+    }
 }
